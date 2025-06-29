129e8a2a70bb427b7fba3ad185f0820163cb6a0e
Bailey Danyluk <baileydanyluk@gmail.com>
2025-06-29 03:43:27 -0600

    add metadata for mission

--- bw/missions/pbo.py
+++ bw/missions/pbo.py
@@ -1,3 +1,4 @@
+import dataclasses
 import shutil
 import tempfile
 import json
@@ -6,9 +7,83 @@ from pathlib import Path
 from bw.subprocess.hemtt import hemtt
 
 
+@dataclasses.dataclass
+class Intel:
+    overview: str
+
+    day: int
+    month: int
+    year: int
+
+    minute: int
+    hour: int
+
+    forecast_waves: float
+
+    start_fog_decay: float
+    forecast_fog_decay: float
+
+    start_weather: float
+    forecast_weather: float
+
+    start_wind: float
+    forecast_wind: float
+
+    is_lighting_forced: bool
+    is_rain_forced: bool
+    is_waves_forced: bool
+    is_wind_forced: bool
+
+
+@dataclasses.dataclass
+class Attribute:
+    name: str
+    expression: str
+    data: dict
+
+
 class MissionFile:
     def __init__(self, mission_as_json: dict):
         self.json = mission_as_json
+        self.custom_attributes = {}
+        self.author = self.json['ScenarioData'].get('author', '')
+        self.mission_name = self.json['sourceName']
+        self.addons = self.json['addons']
+
+        intel = self.json['Mission']['Intel']
+        self.intel = Intel(
+            overview=intel.get('overviewText', ''),
+            day=intel.get('day', 0),
+            month=intel.get('month', 0),
+            year=intel.get('year', 0),
+            minute=intel.get('minute', 0),
+            hour=intel.get('hour', 0),
+            forecast_waves=intel.get('forcecastWaves', 0.0),
+            start_fog_decay=intel.get('startFogDecay', 0.0),
+            forecast_fog_decay=intel.get('forcecastFogDecay', 0.0),
+            start_weather=intel.get('startWeather', 0.0),
+            forecast_weather=intel.get('forecastWeather', 0.0),
+            start_wind=intel.get('startWind', 0.0),
+            forecast_wind=intel.get('forecastWind', 0.0),
+            is_lighting_forced=(1 == intel.get('lightingsForced', 0)),
+            is_rain_forced=(1 == intel.get('rainForced', 0)),
+            is_waves_forced=(1 == intel.get('wavesForced', 0)),
+            is_wind_forced=(1 == intel.get('windForced', 0)),
+        )
+
+        for categories in self.json['CustomAttributes'].values():
+            category_name = categories['name']
+            attribute_count = categories['nAttributes']
+            category_attributes = {}
+            for idx in range(0, attribute_count):
+                attribute = categories[f'Attribute{idx}']
+
+                attribute_name = attribute['property']
+                attribute_expression = attribute.get('expression', '')
+                attribute_data = attribute.get('Value', {})
+                category_attributes[attribute_name] = Attribute(attribute_name, attribute_expression, attribute_data)
+
+            self.custom_attributes[category_name] = category_attributes
 
 
 class MissionLoader:
