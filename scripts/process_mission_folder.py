b4a1377db00be2f5819c85ee4fe1e461b6d68c8e
Bailey Danyluk <baileydanyluk@gmail.com>
2025-07-16 14:38:47 -0600

    update script to be async

--- scripts/process_mission_folder.py
+++ scripts/process_mission_folder.py
@@ -1,16 +1,20 @@
 import os
+import asyncio
 
 from pathlib import Path
 from bw.missions.api import MissionsApi
 
 
 def run(folder: Path):
-    dirpath, _, files = os.walk(folder)
-    for file in files:
-        if not file.endswith('.pbo'):
-            print(f'Skipping non-PBO file: {file}')
-            continue
-        path = Path(dirpath) / file
-        print(f'Processing PBO file: {path}')
-        response = MissionsApi.upload_mission_metadata(path)
-        print(f'Status: {response.status_code}')
+    async def process_folder(folder: Path):
+        dirpath, _, files = os.walk(folder)
+        async with asyncio.TaskGroup() as tg:
+            for file in files:
+                if not file.endswith('.pbo'):
+                    print(f'Skipping non-PBO file: {file}')
+                    continue
+                path = Path(dirpath) / file
+                print(f'Processing PBO file: {path}')
+                tg.create_task(MissionsApi.upload_mission_metadata(path))
+
+    asyncio.run(process_folder(folder))
