43c99c9eaa69fb816ea2488972b134c28056e663
Bailey Danyluk <baileydanyluk@gmail.com>
2025-07-16 14:45:47 -0600

    dont peg cpu

--- scripts/process_mission_folder.py
+++ scripts/process_mission_folder.py
@@ -8,14 +8,18 @@ from bw.missions.api import MissionsApi
 def run(folder: Path):
     async def process_folder(folder: Path):
         api = MissionsApi()
+        tasks = set()
         dirpath, _, files = [x for x in os.walk(folder)][0]
-        async with asyncio.TaskGroup() as tg:
-            for file in files:
-                if not file.endswith('.pbo'):
-                    print(f'Skipping non-PBO file: {file}')
-                    continue
-                path = Path(dirpath) / file
-                print(f'Processing PBO file: {path}')
-                tg.create_task(api.upload_mission_metadata(path))
+        for file in files:
+            if not file.endswith('.pbo'):
+                print(f'Skipping non-PBO file: {file}')
+                continue
+            path = Path(dirpath) / file
+            print(f'Processing PBO file: {path}')
+            tasks.add(asyncio.create_task(api.upload_mission_metadata(path)))
+
+            if len(tasks) >= 10:
+                await asyncio.gather(*tasks)
+                tasks.clear()
 
     asyncio.run(process_folder(folder))
