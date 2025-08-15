e3e542249eafc0f0fdc60bee44a50ca443c57da1
Bailey Danyluk <baileydanyluk@gmail.com>
2025-08-15 01:00:14 -0600

    wrong status

--- scripts/bootstrap_server.py
+++ scripts/bootstrap_server.py
@@ -35,7 +35,7 @@ def create_bot_user(url):
 @request_fixture('v1/auth/login/bot', 'Creating bot session')
 def create_bot_session(url, bot_token):
     response = requests.post(url, json={'bot_token': bot_token})
-    if response.status_code != 201:
+    if response.status_code != 200:
         raise RequestException('Failed to create bot session', response)
     return response.json()
 
