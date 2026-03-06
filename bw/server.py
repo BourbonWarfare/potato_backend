5072d22d871fb2e63d0bcd5bc08ab04002dc4225
Bailey Danyluk <baileydanyluk@gmail.com>
2026-03-05 20:44:32 -0700

    reorganize files

--- bw/server.py
+++ bw/server.py
@@ -38,3 +38,31 @@ def run():
         keyfile=ssl_keyfile_path,
     )
     app.logger.info("that's all, folks")
+
+
+def production():
+    import uvicorn
+
+    if ENVIRONMENT.use_ssl():
+        print('Starting production server with SSL')
+        GLOBAL_CONFIGURATION.require('ssl_ca_certs_path', 'ssl_certfile_path', 'ssl_keyfile_path')
+        ssl_ca_certs_path = GLOBAL_CONFIGURATION['ssl_ca_certs_path']
+        ssl_certfile_path = GLOBAL_CONFIGURATION['ssl_certfile_path']
+        ssl_keyfile_path = GLOBAL_CONFIGURATION['ssl_keyfile_path']
+    else:
+        print('Starting ASGI server !!WITHOUT!! SSL')
+        ssl_ca_certs_path = None
+        ssl_certfile_path = None
+        ssl_keyfile_path = None
+
+    uvicorn.run(
+        'bw.server:app',
+        host='0.0.0.0',
+        port=ENVIRONMENT.port(),
+        ssl_keyfile=ssl_keyfile_path,
+        ssl_certfile=ssl_certfile_path,
+        ssl_ca_certs=ssl_ca_certs_path,
+        log_config=log_config(),
+        log_level='info',
+    )
+    print('thats all, folks')
--- main.py
+++ main.py
@@ -1,36 +1,6 @@
 import bw.server  # noqa: F401
-from bw.server import run
+from bw.server import run, production
 from bw.environment import ENVIRONMENT
-from bw.log import config as log_config
-
-
-def production():
-    from bw.settings import GLOBAL_CONFIGURATION
-    import uvicorn
-
-    if ENVIRONMENT.use_ssl():
-        print('Starting production server with SSL')
-        GLOBAL_CONFIGURATION.require('ssl_ca_certs_path', 'ssl_certfile_path', 'ssl_keyfile_path')
-        ssl_ca_certs_path = GLOBAL_CONFIGURATION['ssl_ca_certs_path']
-        ssl_certfile_path = GLOBAL_CONFIGURATION['ssl_certfile_path']
-        ssl_keyfile_path = GLOBAL_CONFIGURATION['ssl_keyfile_path']
-    else:
-        print('Starting ASGI server !!WITHOUT!! SSL')
-        ssl_ca_certs_path = None
-        ssl_certfile_path = None
-        ssl_keyfile_path = None
-
-    uvicorn.run(
-        'bw.server:app',
-        host='0.0.0.0',
-        port=ENVIRONMENT.port(),
-        ssl_keyfile=ssl_keyfile_path,
-        ssl_certfile=ssl_certfile_path,
-        ssl_ca_certs=ssl_ca_certs_path,
-        log_config=log_config(),
-        log_level='info',
-    )
-    print('thats all, folks')
 
 
 def main():
