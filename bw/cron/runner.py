330b0b769b904f27d65ce1b7714548cc10cf9c51
Bailey Danyluk <baileydanyluk@gmail.com>
2026-03-10 23:05:34 -0600

    debugv2

--- bw/cron/runner.py
+++ bw/cron/runner.py
@@ -83,8 +83,9 @@ class Runner:
     @staticmethod
     def time_to_next_minute() -> float:
         current_time_seconds: float = float(time.monotonic_ns()) / 1e9
-        logger.debug(f'current_time_secs={current_time_seconds}')
-        return 60.0 - (current_time_seconds % 60.0)
+        next_minute_in = 60.0 - (current_time_seconds % 60.0)
+        logger.debug(f'current_time_secs={current_time_seconds}, next_minute_in={next_minute_in}')
+        return next_minute_in
 
     def gather_crons(self):
         root_dir = ENVIRONMENT.cron_path()
