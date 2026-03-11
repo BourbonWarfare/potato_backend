947493f2bd8bb569551a497e9c494ab1eeb310c3
Bailey Danyluk <baileydanyluk@gmail.com>
2026-03-11 00:26:57 -0600

    remove cron debug

--- bw/cron/runner.py
+++ bw/cron/runner.py
@@ -158,7 +158,6 @@ class Runner:
                 now = datetime.datetime.now()
                 async_crons = []
                 async_requests = []
-                logger.debug(f'{", ".join([f"{c.next()}" for c in self.cron_queue_])}')
                 while len(self.cron_queue_) > 0 and self.cron_queue_[0].next() <= now:
                     front = heappop(self.cron_queue_)
                     assert issubclass(front.cron_class, Cron)
