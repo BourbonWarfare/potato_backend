412daebf8c8cf97188f7c816b65c5e2710d44e01
Bailey Danyluk <baileydanyluk@gmail.com>
2026-04-07 18:25:38 -0600

    try to force the import

--- bw/web_event/__init__.py
+++ bw/web_event/__init__.py
@@ -1,1 +1,8 @@
 from bw.web_event.base import BaseEvent as BaseEvent, UniqueEvent as UniqueEvent
+from bw.web_event.discord import DiscordEvent as DiscordEvent
+from bw.web_event.mission import (
+    MissionEvent as MissionEvent,
+    MissionUploadEvent as MissionUploadEvent,
+    IterationCosignedEvent as IterationCosignedEvent,
+    IterationReviewedEvent as IterationReviewedEvent,
+)
