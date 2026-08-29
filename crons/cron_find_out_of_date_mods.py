0f2968ccd579ec227bfda51b3d844fee7b1ca86b
Bailey Danyluk <baileydanyluk@gmail.com>
2026-08-28 23:21:47 -0600

    less frequent cron

--- crons/cron_find_out_of_date_mods.py
+++ crons/cron_find_out_of_date_mods.py
@@ -13,7 +13,7 @@ class FindOutOfDateMods(Cron):
         """
         Returns a cron-encoded string defining when this job will be run next
         """
-        return '*/15 * * * *'
+        return '*/45 6-22 * * *'
 
     async def request(self, session: aiohttp.ClientSession) -> None:
         print('Looking for out-of-date workshop mods')
