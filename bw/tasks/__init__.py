8bf42e1bd3710563fbf4d12fe1ebbe7910bdfb1c
Bailey Danyluk <baileydanyluk@gmail.com>
2026-09-07 21:03:09 -0600

    load tasks

--- bw/tasks/__init__.py
+++ bw/tasks/__init__.py
@@ -1,11 +1,2 @@
-import multiprocessing
-
+from bw.auth.tasks import *
 from bw.tasks.kinds import *
-
-
-def spawn() -> multiprocessing.Process:
-    from bw.tasks.runner import Runner
-
-    process = multiprocessing.Process(target=Runner.run, args=(Runner(),))
-    process.start()
-    return process
