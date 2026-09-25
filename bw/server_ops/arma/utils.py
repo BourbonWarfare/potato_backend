c4582d4221fffd6dfce88c2e57093a19bae5a518
Bailey Danyluk <baileydanyluk@gmail.com>
2026-09-24 22:22:23 -0600

    Dont wrap around on accident

--- bw/server_ops/arma/utils.py
+++ bw/server_ops/arma/utils.py
@@ -31,7 +31,7 @@ async def format_arma_script_error(message: str) -> str:
     error_start = max(0, array_idx - NEGATIVE_OFFSET)
     content_relevant = content[error_start : array_idx + POSITIVE_OFFSET]
 
-    error_line_length = len(content_relevant[array_idx - 1])
+    error_line_length = len(content_relevant[max(0, array_idx - 1)])
     error_line_start = max(0, error_position + ERROR_START)
     error_line_context_length = error_line_length - error_line_start
     content_relevant.insert(NEGATIVE_OFFSET, '-' * error_line_start + '^' * error_line_context_length)
